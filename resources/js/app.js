59926ecc23a8b5465dace499e205a8cdf76f156b
Larissa Oliveira <larissa-ocds@hotmail.com>
2023-03-13 15:57:22 -0300

    chore: ➕ add dependência fomantic-ui , jquery

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,2 @@
 import './bootstrap';
+import 'fomantic-ui/dist/semantic.min';
--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -3,10 +3,10 @@
  * to our Laravel back-end. This library automatically handles sending the
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
-
+import $ from "jquery";
 import axios from 'axios';
 window.axios = axios;
-
+window.$ = window.jQuery = $;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /**
